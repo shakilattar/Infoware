43e7c69e8dd48b93639ca0952bee5cdfd2441dbd
Shakil Attar <shakilattar@Shakils-MacBook-Pro.local>
2022-09-23 23:40:14 +0530

    added stocks activity

--- demo/lib/home_screen.dart
+++ demo/lib/home_screen.dart
@@ -283,17 +283,18 @@ class _HomeScreenState extends State<HomeScreen> {
                                       ),
                                     ],
                                   ),
-                                  SizedBox(
-                                      width: _mediaQuery.size.width * 0.09),
                                   Container(
                                     padding: EdgeInsets.only(
-                                        left: 0, right: 7, top: 24, bottom: 0),
-                                    child: Text(
-                                      '↑ 5.49%',
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromRGBO(96, 200, 158, 1),
+                                        left: 4, right: 0, top: 0, bottom: 0),
+                                    child: Container(
+                                      child: ClipRRect(
+                                        child: Image(
+                                            height:
+                                                _mediaQuery.size.height * 0.08,
+                                            width:
+                                                _mediaQuery.size.width * 0.28,
+                                            image: AssetImage(
+                                                'assets/trading.png')),
                                       ),
                                     ),
                                   ),
@@ -312,6 +313,228 @@ class _HomeScreenState extends State<HomeScreen> {
                           shape: RoundedRectangleBorder(
                               side: BorderSide(color: Colors.black12),
                               borderRadius: BorderRadius.circular(18)),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.only(
+                                        left: 24, right: 8, top: 24, bottom: 0),
+                                    child: CircleAvatar(
+                                      radius: 18,
+                                      backgroundColor: Colors.white,
+                                      backgroundImage:
+                                          AssetImage('assets/brand.png'),
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.only(
+                                        left: 2, right: 0, top: 24, bottom: 0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'TSLA',
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Tesla',
+                                          style: TextStyle(
+                                            fontSize: 10,
+                                            color: Colors.black45,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                      width: _mediaQuery.size.width * 0.09),
+                                  Container(
+                                    padding: EdgeInsets.only(
+                                        left: 0, right: 7, top: 24, bottom: 0),
+                                    child: Text(
+                                      '↑ 5.49%',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromRGBO(96, 200, 158, 1),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: _mediaQuery.size.height * 0.02,
+                              ),
+                              Row(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.only(
+                                            left: 24,
+                                            right: 8,
+                                            top: 24,
+                                            bottom: 0),
+                                        child: Text(
+                                          '25.68',
+                                          style: TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(
+                                            left: 24,
+                                            right: 8,
+                                            top: 2,
+                                            bottom: 0),
+                                        child: Text(
+                                          '\$909.68',
+                                          style: TextStyle(
+                                            color: Colors.black45,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.only(
+                                        left: 4, right: 0, top: 0, bottom: 0),
+                                    child: Container(
+                                      child: ClipRRect(
+                                        child: Image(
+                                            height:
+                                                _mediaQuery.size.height * 0.08,
+                                            width:
+                                                _mediaQuery.size.width * 0.28,
+                                            image: AssetImage(
+                                                'assets/trading.png')),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding:
+                    EdgeInsets.only(left: 20, right: 6, top: 22, bottom: 13),
+                child: Row(
+                  children: [
+                    Text(
+                      'Stocks Activity',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: _mediaQuery.size.height * 0.11,
+                width: _mediaQuery.size.width * 0.90,
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black12),
+                      borderRadius: BorderRadius.circular(18)),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(
+                            left: 24, right: 8, top: 24, bottom: 24),
+                        child: CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.white,
+                          backgroundImage: AssetImage('assets/nvidia.png'),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                            left: 10, right: 8, top: 24, bottom: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'NVDA',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: _mediaQuery.size.height * 0.004),
+                            Text(
+                              'Nvidia',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black45,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                            left: 40, right: 22, top: 24, bottom: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '25.94%',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(96, 200, 158, 1),
+                              ),
+                            ),
+                            SizedBox(height: _mediaQuery.size.height * 0.003),
+                            Text(
+                              '↑ 0.14%',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromRGBO(96, 200, 158, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                            left: 20, right: 10, top: 24, bottom: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '\$227.26',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: _mediaQuery.size.height * 0.004),
+                            Text(
+                              '10 Shares',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black45,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
